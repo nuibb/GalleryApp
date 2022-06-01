de31852b9f8f0ea13dbdced620657af45249a87a
Nurul Islam <nuibb.hasan@gmail.com>
2022-06-02 05:16:34 +0600

    feat: adds default app template for iOS from Xcode
    BREAKING CHANGE: `default` template for iOS is included for baseline commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by ReliSource Technologies Ltd. on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
